3e3efb339431a6f194b29061a9779402fb52191a
Im <91757003+imimsan@users.noreply.github.com>
2025-04-26 15:01:15 +0900

    Update main_test.go

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -5,6 +5,6 @@ import "testing"
 func TestEvenOrOdd(t *testing.T) {
     result := EvenOrOdd(10)
     if result != "even" {
-        t.Errorf("expected:a even, actual: %s", result)
+        t.Errorf("expected:a ev1en, actual: %s", result)
     }
 }
